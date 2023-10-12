4aa7ebbb84142c472f8d668b4ed7cad7439bc3d2
Thato <t.chitja369@gmail.com>
2023-10-12 13:27:58 +0200

    Create Companies Controller and View

---
+++ Licensing/Licensing/Controllers/CompaniesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Licensing.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
