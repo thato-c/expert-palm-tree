3d3865735e7a03b429e3f09975936890fa7cac83
Thato <t.chitja369@gmail.com>
2023-10-12 11:52:31 +0200

    Add Licence Model

---
+++ Licensing/Licensing/Models/Licence.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Licensing.Models
+{
+    public class Licence
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Cost { get; set; }
+    }
+}
