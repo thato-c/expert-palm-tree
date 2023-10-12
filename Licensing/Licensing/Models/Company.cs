7483625871afb00a12bb83e25328eec9b637c7dd
Thato <t.chitja369@gmail.com>
2023-10-12 11:43:54 +0200

    Add Company Model

---
+++ Licensing/Licensing/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Licensing.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
