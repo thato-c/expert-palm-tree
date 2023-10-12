8889b6a9a48c303b416c8e824771d3cfa3e9b2ba
Thato <t.chitja369@gmail.com>
2023-10-12 13:53:11 +0200

    Display list of companies or an error message when there are no companies

--- Licensing/Licensing/Controllers/CompaniesController.cs
+++ Licensing/Licensing/Controllers/CompaniesController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Licensing.Data;
+using Licensing.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Licensing.Controllers
 {
     public class CompaniesController : Controller
     {
+        private readonly LicenceContext _context;
+
+        public CompaniesController(LicenceContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Retrieve a list of companies from the database
+            var companies = _context.Companies.ToList();
+
+            if (companies.Count == 0)
+            {
+                ViewBag.Message = "No Companies Registered.";
+                return View();
+            }
+            
+            return View(companies);
         }
     }
 }
