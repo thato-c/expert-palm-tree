616d3fdf377d34aa481f41de39c730b1a7bb13a1
Thato <t.chitja369@gmail.com>
2023-10-13 11:44:34 +0200

    Add Company Update View

--- Licensing/Licensing/Controllers/CompaniesController.cs
+++ Licensing/Licensing/Controllers/CompaniesController.cs
@@ -2,6 +2,7 @@
 using Licensing.Models;
 using Licensing.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Licensing.Controllers
 {
@@ -56,6 +57,24 @@ namespace Licensing.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id) 
+        {
+            if (id == null || _context.Companies == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies.FirstOrDefaultAsync(c => c.CompanyId == id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
         }
     }
 }
