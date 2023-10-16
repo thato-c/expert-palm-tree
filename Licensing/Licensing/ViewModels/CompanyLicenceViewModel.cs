df5c62ade5e2e84f0e6b5bae799547fc88d12268
Thato <t.chitja369@gmail.com>
2023-10-17 00:43:58 +0200

    Trying to revert to previous state

---
+++ Licensing/Licensing/ViewModels/CompanyLicenceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Licensing.ViewModels
+{
+    public class CompanyLicenceViewModel
+    {
+        public CompanyViewModel CompanyDetails { get; set; }
+
+        // Licence purchase information
+        [Required(ErrorMessage = "Licence is required")]
+        public int LicenceId { get; set; }
+
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than or equal to 1")]
+        public int Quantity { get; set; }
+
+        public SelectList LicenceSelectList { get; set; }
+    }
+}
